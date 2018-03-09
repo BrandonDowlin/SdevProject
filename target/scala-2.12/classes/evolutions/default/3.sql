57aa31cbee5bae9d2cd4ffc9cb014e42199f39ba
Brandon <brandandowling@yahoo.com>
2018-03-09 11:33:34 +0000

    Third times the charm

--- target/scala-2.12/classes/evolutions/default/3.sql
+++ target/scala-2.12/classes/evolutions/default/3.sql
@@ -1,8 +1,7 @@
 # --- !Ups
 delete from user;
 
-insert into user (email,name,password,role) values ( 'admin@products.com', 'Alice Admin', 'password', 'admin' );
+insert into user (email,name,password,role) values ( 'admin@employees.com', 'Brandon Admin', 'password', 'admin' );
 
-insert into user (email,name,password,role) values ( 'manager@products.com', 'Bob Manager', 'password', 'manager' );
+insert into user (email,name,password,role) values ( 'manager@employees.com', 'Ronan Manager', 'password', 'manager' );
 
-insert into user (email,name,password,role) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer' );
